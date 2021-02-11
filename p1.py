c50c477a8583913de05bdabdd532c73e248c97d5
Aiswarya-Baburaj <aiswarya@deerom.com>
2021-02-11 12:23:50 +0530

    third class

--- p1.py
+++ p1.py
@@ -7,3 +7,10 @@ print(modelx.max_speed,modelx.mileage)
 
 class Vehicle:
     pass
+class Vehicle():
+	def __init__(self,name,max_speed,mileage):
+		self.name=name
+		self.max_speed=max_speed
+		self.mileage=mileage
+class Bus(Vehicle):
+	pass
