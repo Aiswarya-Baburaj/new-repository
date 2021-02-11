6b8bb45081b78cb298f1122dd3983fdffa54be1c
Aiswarya-Baburaj <aiswarya@deerom.com>
2021-02-11 12:39:45 +0530

    fifth class

--- p1.py
+++ p1.py
@@ -18,3 +18,12 @@ class Bus(Vehicle):
 School_bus = Bus("School Volvo", 180, 12)
 print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
 
+class Vehicle:
+	def __init__(self,name,max_speed,mileage):
+		self.name=name
+		self.max_speed=max_speed
+		self.mileage=mileage
+	def seatingcapacity(self,capacity):
+		return f"the seating capacity of a {self.name} is {capacity} passengers"
+seating capacity of a bus is 50 passengers		
+		
